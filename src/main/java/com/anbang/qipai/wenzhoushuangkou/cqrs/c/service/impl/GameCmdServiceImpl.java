e533cd5c9b6a020234036f9ce0caafe4fbc80417
林少聪 < @PC-20180515PRDG>
2019-04-29 17:46:16 +0800

    bugfix

--- src/main/java/com/anbang/qipai/wenzhoushuangkou/cqrs/c/service/impl/GameCmdServiceImpl.java
+++ src/main/java/com/anbang/qipai/wenzhoushuangkou/cqrs/c/service/impl/GameCmdServiceImpl.java
@@ -22,6 +22,7 @@ import com.dml.mpgame.game.extend.vote.OnlineVotePlayersFilter;
 import com.dml.mpgame.game.extend.vote.VoteOption;
 import com.dml.mpgame.game.join.FixedNumberOfPlayersGameJoinStrategy;
 import com.dml.mpgame.game.leave.HostGameLeaveStrategy;
+import com.dml.mpgame.game.leave.NoPlayerCancelGameGameLeaveStrategy;
 import com.dml.mpgame.game.leave.OfflineAndNotReadyGameLeaveStrategy;
 import com.dml.mpgame.game.leave.OfflineGameLeaveStrategy;
 import com.dml.mpgame.game.leave.PlayerGameLeaveStrategy;
@@ -322,7 +323,7 @@ public class GameCmdServiceImpl extends CmdServiceBase implements GameCmdService
 		newGame.setLeaveByHangupStrategyBeforeStart(new OfflineAndNotReadyGameLeaveStrategy());
 
 		newGame.setLeaveByPlayerStrategyAfterStart(new OfflineGameLeaveStrategy());
-		newGame.setLeaveByPlayerStrategyBeforeStart(new PlayerGameLeaveStrategy());
+		newGame.setLeaveByPlayerStrategyBeforeStart(new NoPlayerCancelGameGameLeaveStrategy());
 
 		newGame.setBackStrategy(new OnlineGameBackStrategy());
 		newGame.create(gameId, playerId);
